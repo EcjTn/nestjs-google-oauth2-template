8aa29b17851e082bbfa86dafaee814d3877ffa1d
EcjTn <ecjtaneo@gmail.com>
2025-11-09 19:41:26 +0800

    feat(module): Added typeorm with configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncOption } from './configs/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync(typeOrmAsyncOption),
     UsersModule,
     AuthModule,
   ],
